61fd33be25879316d1329e1f8fa86e4fd5153871
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-18 01:28:36 +0400

    checking if this works

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     names = dir(hidden_4)
     strlen = len(names)
     for name in names:
-        if(name[:2] != "__"):
+        if name[:2] != "__":
             print(name)
