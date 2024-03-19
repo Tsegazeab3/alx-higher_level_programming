d1040104787c6e631ad6650208530c52322e9ce8
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-19 22:46:27 +0400

    don't forget how to count

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 # function retrieves an elenment from a list
 # idx is the index of the number
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
     else:
         return my_list[idx]
