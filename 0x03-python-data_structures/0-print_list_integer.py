1e60228ed4cc918b91446d52e84ba67634112a76
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-19 22:32:50 +0400

    checking :d can make that much of a difference

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for number in my_list:
-        print("{}".format(number))
+        print("{:d}".format(number))
 if __name__ == "__main__":
     print_list_integer()
