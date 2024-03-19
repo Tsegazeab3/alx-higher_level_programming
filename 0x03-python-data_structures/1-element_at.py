43dcc0f21320ac66001a8e703aeb6f2fe00159e6
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-19 22:41:26 +0400

    checking if this works

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# function retrieves an elenment from a list
+# idx is the index of the number
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list):
+        return None
+    else:
+        return my_list[idx]
+if __name__ == "__main__":
+    element_at()
