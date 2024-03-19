f6cf2b9457cdc498a9b93c250b4e4ff8336697b4
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-19 22:53:10 +0400

    checking if this works

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
+
+
+if __name__ == "__main__":
+    replace_in_list()
