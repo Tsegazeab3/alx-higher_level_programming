4a5e6d06637eaea5432a78ba5e082288907495a1
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-21 12:45:43 +0400

    don't assign while using methods they return none

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list = []):
-    my_list = my_list.reverse()
+def print_reversed_list_integer(my_list=[]):
+    my_list.reverse()
     for number in my_list:
         print("{:d}".format(number))
