dbd0e5fd223880763f6da0ca0ce00a5b52ebaade
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-22 10:59:19 +0400

    i am seeing the difference between return and return None

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if not my_list :
-        list_copy = my_list.copy()
-        if (idx < 0) or (idx > (len(my_list) - 1)):
-            return(list_copy)
-        else:
-            list_copy[idx] = element
-            return(list_copy)
+    if my_list == None:
+        return
+    list_copy = my_list.copy()
+    if (idx < 0) or (idx > (len(my_list) - 1)):
+        return(list_copy)
     else:
-        return None
+        list_copy[idx] = element
+        return(list_copy)
