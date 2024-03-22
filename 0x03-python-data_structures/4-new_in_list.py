f3e8c96482287bec4ccb4e32f2358c5e53d35662
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-22 10:55:59 +0400

    trying to put is not my_list instead of mylist!=None

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list is not None:
+    if not my_list :
         list_copy = my_list.copy()
         if (idx < 0) or (idx > (len(my_list) - 1)):
             return(list_copy)
