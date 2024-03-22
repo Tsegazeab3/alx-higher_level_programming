6a81f8dade8dabcda5d0989c4a825fda4d566ac4
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-03-22 11:00:47 +0400

    tyring to see if the problem is the .copy method

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     if my_list == None:
         return
-    list_copy = my_list.copy()
+    list_copy = my_list[:]
     if (idx < 0) or (idx > (len(my_list) - 1)):
         return(list_copy)
     else:
