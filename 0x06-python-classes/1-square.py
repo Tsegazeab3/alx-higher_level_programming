00fe0adcd7c21f6a4032501d3954abc3bfdcd903
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-04-06 23:21:55 +0400

    did the second question

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""writing a Square that defines a square(based on 0-square.py)"""
+
+
+class Square:
+    """Represent a Square. """
+
+
+    def __init__(self,size) -> None:
+        """Initializing a new square
+
+		Args:
+            size(Int): the size of the new square
+		"""
+        self.__size = size
