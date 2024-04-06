b6161d7a4ddf294d28d91b62e0b1cb6fa5861d07
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-04-07 00:23:53 +0400

    removed the setters and getters and used simple if clauses inside the init function

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,51 +1,16 @@
-# #!/usr/bin/bash
-# """ write a Square class with:
-# -private instance attribute: size
-# -encapsulate size and insert setter with Type Error if not integer
-# and Value Error if size < 0
-# """
-
-
-# class Square:
-#     """ Defining Square"""
-#     def __init__(self, size=0):
-#         """intitialization function for Square
-
-#         Args:
-#             size(int): The size of the new square
-#         """
-#         self.size = size
-
-#     @property
-#     def size(self):
-#         """getter function for instance"""
-#         return(self.__size)
-
-#     @size.setter
-#     def size(self, value):
-#         """ setter function for size
-
-#         Args:
-#             value(int): value to be inserted"""
-#         if type(value) != int:
-#             raise TypeError("size must be an integer")
-#         if(value < 0):
-#             raise ValueError("size must be >= 0")
-#         self.__size = value
 #!/usr/bin/python3
-"""Define a class Square."""
+""" a module that creates a class named square"""
 
 
 class Square:
-    """Represent a square."""
-
+    """ attributes of a class"""
     def __init__(self, size=0):
-        """Initialize a new Square.
+        """initiate a new square
 
         Args:
-            size (int): The size of the new square.
+            size(int): is not an integer
         """
-        if not isinstance(size, int):
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
