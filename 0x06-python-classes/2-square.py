c55c5232b718b19a81a529d9b93a44679c187f87
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-04-07 17:33:22 +0400

    don't use type(value) == data type

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,16 +4,18 @@
 
 class Square:
     """ attributes of a class"""
+
     def __init__(self, size=0):
         """initiate a new square
 
         Args:
             size(int): is not an integer
+
         Raises:
             TypeError: if size not an int
             ValueEror: if size less than 0
         """
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
