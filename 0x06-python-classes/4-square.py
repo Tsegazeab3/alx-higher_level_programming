2deafae3dd4fe2612a9d99b7a6801ed8f8575371
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-04-07 17:10:43 +0400

    checking what exactly i got wrong on question 3

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -23,8 +23,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        self.size = value
 
     def area(self):
         """ area of the square"""
-        return(self.__size ** 2)
+        return(self.size ** 2)
