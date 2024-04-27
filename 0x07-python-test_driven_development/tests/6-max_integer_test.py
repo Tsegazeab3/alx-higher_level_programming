d0fa39c93a174a59a259b296f304346130a60f45
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-04-27 14:27:43 +0400

    learned somethings about unittest

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import unittest
+max_no = __import__("6-max_integer").max_integer
+"""
+This module is a test file for 6-max_integer
+This module is a test file for 6-max_integer
+This module is a test file for 6-max_integer
+This module is a test file for 6-max_integer
+This module is a test file for 6-max_integer
+"""
+class Test_max_integer(unittest.TestCase):
+    """
+    class containing test cases
+    inherited from unittest.testcase
+    """
+    def cor_output (self):
+        """
+        checks the correct out puts
+        """
+        """ tests easy numbers"""
+        self.assertAlmostEqual(max_no([1, 2, 3]), 3)
+        """ tests letters with ascii code"""
+        self.assertAlmostEqual(max_no("abc"), 'c')
+        """ tests empty list"""
+        self.assertAlmostEqual(max_no([]), None)
+        """tests hexa numbers"""
+        self.assertAlmostEqual([0x01, 0x4, 0x2], 0x02)
+        """tests floats"""
+        self.assertAlmostEqual([0.3, -0.3, 5.2], 5.2)
--- 0x07-python-test_driven_development/trial.py
+++
@@ -1,7 +0,0 @@
-def text_indentation(text):
-    punctuations = ".?:"
-    for char in text:
-        if char != ' ':
-            print(char, end="")
-        if char in punctuations:
-            print('\n')
