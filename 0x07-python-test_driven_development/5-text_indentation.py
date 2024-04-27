7db5cb81e03d86aeb7b2e423479bc443b5b9da4f
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-04-27 04:38:44 +0400

    fixed the code style and added the comments

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
+"""
+This module containts the text_indent function
+the test file is inside tests as 5-text_indentation
+it has not implement no other modules
+"""
+
+
 def text_indentation(text):
-    if not isinstance(text,str):
+    """
+    text_indentation function:
+        indents text
+    Args:
+        text to be indented
+    exceptions:
+        Type Error: text must be a string only
+    """
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
     i = 0
-    while i <= len(text) - 1 :
+    while i <= len(text) - 1:
         if text[i] in ".?:":
             print('{}\n'.format(text[i]))
             i += 1
-            while i < len(text) and text[i]==' ':
+            while i < len(text) and text[i] == ' ':
                 i += 1
                 continue
         else:
-            print(text[i],end="")
+            print(text[i], end="")
             i += 1
