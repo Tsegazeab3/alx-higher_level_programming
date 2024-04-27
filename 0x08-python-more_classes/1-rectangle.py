29c1cfc6dfd967a65b8e74c381085898894bfbc3
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-04-27 17:24:09 +0400

    removed the run module

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -70,6 +70,3 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
