cd05ebea36148a43f9bf5deb512876803898d1a4
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-05-15 00:09:26 +0400

    python is easy

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" prints a sorted list"""
+
+
+class MyList(list):
+    """ inherits from class"""
+    def print_sorted(list):
+        """ prints a sorted list """
+        print(sorted(list))
