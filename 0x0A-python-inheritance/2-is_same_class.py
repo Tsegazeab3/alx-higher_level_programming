c260bf87046aa4530b934f731787538ffd869367
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-05-22 14:29:17 +0400

    what is wrong

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,8 +1,5 @@
-# Write a function that returns True if the object is exactly an instance of the specified class ; otherwise False.
 #!/usr/bin/python3
+"""Module for is_same_class"""
 def is_same_class(obj, a_class):
-    """Check if obj is exactly an instance of a_class"""
+    """Function that returns True if the object is exactly an instance of the specified class; otherwise False."""
     return type(obj) == a_class
-if __name__ == ("__main__"):
-    import doctest
-    doctest.testfile("tests/2-is_same_class.txt")
