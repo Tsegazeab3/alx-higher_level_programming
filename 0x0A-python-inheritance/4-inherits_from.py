b88be1302e352f2fa4ce37542caa399063ef0bf8
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-05-22 15:56:40 +0400

    checking something

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module contains the inherits_from function."""
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise
+    False."""
+    return isinstance(obj, a_class)
