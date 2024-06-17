4269d296c66c451ae16b27f02872099ce63cf13e
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-06-17 17:03:53 +0400

    something is wrong with the commenting

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""this module contains a function that returns True if the object is an"""
+"""
+this module contains a function that returns True if the object is an
+"""
 
 
 def is_kind_of_class(obj, a_class):
