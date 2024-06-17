2ccd31f8553c7238f0f48630b9e29ae5f61d382b
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-06-17 17:02:44 +0400

    added a bracket at the end of the return value

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -7,4 +7,4 @@ def is_kind_of_class(obj, a_class):
     instance of a class that inherited from, the specified class ; otherwise
     False."""
 
-    return isinstance(obj, a_class)
+    return (isinstance(obj, a_class))
