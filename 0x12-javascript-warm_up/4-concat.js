69fcda672c802620d9d5902fb120c7c62380952d
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-06-19 01:26:39 +0400

    I had to fix question 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv[2] && argv[3]) {
-  console.log(`${argv[2]} is ${argv[3]}`);
-} else {
-  console.log('No argument');
-}
+console.log(`${argv[2]} is ${argv[3]}`);
